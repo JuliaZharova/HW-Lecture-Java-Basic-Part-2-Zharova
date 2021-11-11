87d841cad6778297977344aff18f2b47a2423c00
Julia Zharova <julia.amp.zharova@gmail.com>
2021-11-11 13:47:48 +0200

    post-check changes

--- MySwitchCalc/src/com/company/Main.java
+++ MySwitchCalc/src/com/company/Main.java
@@ -1,35 +1,57 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         double val1 = 0;
         double val2 = 0;
         String operation;
+        String supportOperations = " + - * / %";
         Scanner scanner = new Scanner(System.in);
+        boolean cycle = true;
 
-        System.out.print("Please enter the value of the first variable: ");
-        try {
-            val1 = scanner.nextDouble();
-        }
-        catch (Exception exception) {
-            System.out.println("Please enter a valid number.");
-            return;
-        }
+        do {
+            System.out.println("Please enter the value of the first variable: ");
+            if (!scanner.hasNextDouble()) {
+                System.out.println("Please enter a valid number.");
+                scanner.next();
+            } else {
+                val1 = scanner.nextDouble();
+                cycle = false;
+            }
 
-        System.out.print("Please enter the required operator. Supported operations: +, -, *, /, %: ");
-        operation = scanner.next();
+        } while (cycle);
 
-        System.out.print("Please enter the value of the second variable: ");
-        try {
-            val2 = scanner.nextDouble();
-        }
-        catch (Exception exception) {
-            System.out.println("Please enter a valid number.");
-            return;
-        }
+        cycle = true;
+
+        do {
+            System.out.println("Please enter the required operator. Supported operations: +, -, *, /, %: ");
+            operation = scanner.next();
+            if (!supportOperations.contains(operation)) {
+                System.out.println("Please use only supported operators. ");
+            } else {
+                //operation = scanner.next();
+                cycle = false;
+            }
+
+        } while (cycle);
+
+        cycle = true;
+
+        do {
+            System.out.println("Please enter the value of the second variable: ");
+            if (!scanner.hasNextDouble()) {
+                System.out.println("Please enter a valid number.");
+                scanner.next();
+            } else {
+                val2 = scanner.nextDouble();
+                cycle = false;
+            }
+
+        } while (cycle);
 
         System.out.print("Result: ");
         switch (operation) {
